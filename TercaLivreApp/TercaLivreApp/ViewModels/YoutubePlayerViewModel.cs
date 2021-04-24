1f5d59dc445fca3df19b71be5825c462b637ef8a
Eduardo Alves Canuto <edutecni@gmail.com>
2021-04-24 17:56:54 -0300

    Implement YoutubePlayer

---
+++ TercaLivreApp/TercaLivreApp/ViewModels/YoutubePlayerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using TercaLivreApp.Models;
+using TercaLivreApp.Views;
+using Xamarin.Forms;
+
+namespace TercaLivreApp.ViewModels
+{
+    public class YoutubePlayerViewModel : BaseViewModel
+    {
+        public YoutubePlayerViewModel()
+        {
+            Title = "YoutubePlayer";            
+        }        
+    }
+}
---
+++ TercaLivreApp/TercaLivreApp/Views/YoutubePlayerPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using TercaLivreApp.ViewModels;
+using Xamarin.Forms;
+using Google.Apis.YouTube.v3;
+
+namespace TercaLivreApp.Views
+{
+    public partial class YoutubePlayerPage : ContentPage
+    {
+        YoutubePlayerViewModel _viewModel;
+
+        public YoutubePlayerPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new YoutubePlayerViewModel();
+
+            //YouTubeService youTubeService = new YouTubeService();
+            //YoutubePlayerPage youtubePlayerPage = new YoutubePlayerPage();
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
